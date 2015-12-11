8b3026a4a21f1cf8ad32b87321b3c908d056330d
yuba <janeco3@gmail.com>
2015-12-12 01:04:18 +0900

    UserのコメントにDDLを追加

--- src/main/java/com/example/entity/User.kt
+++ src/main/java/com/example/entity/User.kt
@@ -6,6 +6,16 @@ import javax.persistence.Id
 import javax.persistence.Table
 
 /**
+ * DDL:
+ * <pre>
+ * CREATE TABLE `user` (
+ * `id` int(11) NOT NULL AUTO_INCREMENT,
+ * `name` varchar(255) DEFAULT NULL,
+ * `sex` char(1) DEFAULT NULL,
+ * PRIMARY KEY (`id`)
+ * ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1
+ * </pre>
+ *
  * @Anther yubaken
  */
 @Entity
